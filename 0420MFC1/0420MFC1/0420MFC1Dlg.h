93156d89f1782edb058e6276d7fbf45119281067
balabala120 <1804574837@qq.com>
2020-04-28 14:57:33 +0800

    在一个基于对话框的程序中，实现计算器的基本功能：＋－× ÷  开平方，求倒数，并且能对参与运算的数进行合法性检查

---
+++ 0420MFC1/0420MFC1/0420MFC1Dlg.h
@@ -0,0 +1,44 @@
+
+// 0420MFC1Dlg.h : 头文件
+//
+
+#pragma once
+
+
+// CMy0420MFC1Dlg 对话框
+class CMy0420MFC1Dlg : public CDialogEx
+{
+// 构造
+public:
+	CMy0420MFC1Dlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MY0420MFC1_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	CString A;
+	CString B;
+	CString C;
+	afx_msg void OnBnClickedButton1();
+	afx_msg void OnBnClickedButton2();
+	afx_msg void OnBnClickedButton3();
+	afx_msg void OnBnClickedButton4();
+	afx_msg void OnBnClickedButton5();
+	afx_msg void OnBnClickedButton6();
+};
